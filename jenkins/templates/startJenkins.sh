794612acefdde5d34f43d5338ff4ea21b3087014
Nhan Tran <tranphanquocnhan@gmail.com>
2014-07-22 02:00:26 +0700

    Updated script to create Jenkins job dynamically

--- jenkins/templates/startJenkins.sh
+++ jenkins/templates/startJenkins.sh
@@ -4,4 +4,6 @@ CONFIG=/home/jenkins/.jenkins/org.jfrog.hudson.ArtifactoryBuilder.xml
 
 sed -i -e "s/<url></<url>http:\/\/$ARTIFACTORY_PORT_8081_TCP_ADDR:$ARTIFACTORY_PORT_8081_TCP_PORT\/artifactory</g" $CONFIG
 
+ssh-keyscan $GITOLITE_PORT_22_TCP_ADDR >> ~/.ssh/known_hosts
+
 java -jar /opt/jenkins.war
