a199a56af64323ca387fd8ca6100e977b8321a96
Nhan Tran <tranphanquocnhan@gmail.com>
2014-07-29 17:18:00 +0700

    Connected to Sonar

--- jenkins/templates/startJenkins.sh
+++ jenkins/templates/startJenkins.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-CONFIG=/home/jenkins/.jenkins/org.jfrog.hudson.ArtifactoryBuilder.xml
+# Configure Artifactory plugin
+sed -i -e "s|<url><|<url>http://$ARTIFACTORY_PORT_8081_TCP_ADDR:$ARTIFACTORY_PORT_8081_TCP_PORT/artifactory<|g" /home/jenkins/.jenkins/org.jfrog.hudson.ArtifactoryBuilder.xml
 
-sed -i -e "s/<url></<url>http:\/\/$ARTIFACTORY_PORT_8081_TCP_ADDR:$ARTIFACTORY_PORT_8081_TCP_PORT\/artifactory</g" $CONFIG
+# Configure Sonarqube plugin
+sed -i -e "s|<serverUrl><|<serverUrl>http://$SONARQUBE_PORT_9000_TCP_ADDR:$SONARQUBE_PORT_9000_TCP_PORT<|g" /home/jenkins/.jenkins/hudson.plugins.sonar.SonarPublisher.xml
+sed -i -e "s|<databaseUrl><|<databaseUrl>jdbc:mysql://$DB_PORT_3306_TCP_ADDR:$DB_PORT_3306_TCP_PORT/sonar<|g" /home/jenkins/.jenkins/hudson.plugins.sonar.SonarPublisher.xml
 
+# Accept ssh connections from Gitolite host
 ssh-keyscan $GITOLITE_PORT_22_TCP_ADDR >> ~/.ssh/known_hosts
 
 java -jar /opt/jenkins.war
